3c4af279cc8110a2c935fe1c7de3c15bd836ea2c
Kendall Tran <kendalltran@gmail.com>
2019-04-22 18:15:50 -0400

    Initial commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+//this code is getting run in the background/async
+console.log('Service Worker: Registered');
+self.addEventListener('install', function (event) {
+	event.waitUntil(
+		caches.open('app-static-v1').then(function (cache) {
+			return cache.addAll([
+				'/',
+				'css/styles.css',
+				'img/1.jpg',
+				'img/2.jpg',
+				'img/3.jpg',
+				'img/4.jpg',
+				'img/5.jpg',
+				'img/6.jpg',
+				'img/7.jpg',
+				'img/8.jpg',
+				'img/9.jpg',
+				'img/10.jpg',
+				'js/dbhelper.js',
+				'js/main.js',
+				'js/restaurant_info.js',
+				'index.html',
+				'restaurant.html',
+        'data/restaurants.json'
+			]);
+		})
+	);
+});
+
+// self.addEventListener('fetch', function (event) {
+//   console.log('test1');
+// 	event.respondWith(
+// 		caches.match(event.request).then(function (response) {
+// 			if (response) return response;
+// 			return fetch(event.request);
+// 		})
+// 	);
+// });
